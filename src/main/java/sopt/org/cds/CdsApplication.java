41670a58de8ea0504cd63ddd20bec76eb82019a8
chaerin <achrvvv@gmail.com>
2023-05-14 23:53:29 +0900

    initialize project

---
+++ src/main/java/sopt/org/cds/CdsApplication.java
@@ -0,0 +1,13 @@
+package sopt.org.cds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sopt/org/cds/CdsApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.org.cds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
